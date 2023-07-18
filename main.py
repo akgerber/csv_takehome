92e43b6d6b3b1d55659eefe6ee8609df9f685568
Alan Gerber <akgerber@users.noreply.github.com>
2023-07-18 15:02:01 -0400

    Argument parsing

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-def main():
-    print("Hello world!")
+#!/usr/bin/env -S python3
+
+import argparse
+
+def main(csv_filename: str, search_query: str):
+    print(csv_filename)
+    print(search_query)
+
     return 1
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        prog="CSV Searcher"
+    )
+
+    parser.add_argument('filename')
+    parser.add_argument('search_query')
+    args = parser.parse_args()
+
+    main(csv_filename=args.filename, search_query=args.search_query)
