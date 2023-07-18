c77f17c700ef5538a00b2e91cce5825891dac44f
Alan Gerber <akgerber@users.noreply.github.com>
2023-07-18 16:14:13 -0400

    Part 1

--- main.py
+++ main.py
@@ -1,12 +1,82 @@
 #!/usr/bin/env -S python3
 
 import argparse
+import csv
+from dataclasses import dataclass
 
-def main(csv_filename: str, search_query: str):
-    print(csv_filename)
-    print(search_query)
+@dataclass
+class MatchOperator:
+    column_name: str
+    matching_value: str
 
-    return 1
+
+QueryNode = MatchOperator | None
+
+@dataclass
+class ParsedQuery:
+    tree: QueryNode
+
+
+@dataclass
+class Row:
+    raw: str
+    row_dict: dict
+
+
+def parse_match_args(args: str) -> MatchOperator | None:
+    match_args = args.split(",")
+    if len(match_args) != 2:
+        return False
+    return MatchOperator(
+        column_name=match_args[0].strip('" '), matching_value=match_args[1].strip('" '))
+
+def parse_query(search_query: str) -> ParsedQuery | None:
+    tree = None
+    match search_query.split("("):
+        case "MATCH", body:
+            match = parse_match_args(body[:-1])
+            if match is None:
+                return None
+            tree = match
+        case _:
+            return None
+
+    return ParsedQuery(tree=tree)
+    
+def eval_search_query(row: Row, search_query: ParsedQuery) -> bool:
+    if type(search_query.tree) == MatchOperator:
+        matcher: MatchOperator = search_query.tree
+        return matcher.column_name in row.row_dict \
+            and row.row_dict[matcher.column_name] == matcher.matching_value
+    else:
+        return False
+    
+
+def main(csv_filename: str, search_query: str) -> bool:
+    query = parse_query(search_query)
+    if query is None:
+        print(f"Cannot parse query: {search_query}")
+        return False
+
+    try: 
+        with open(csv_filename, newline='') as f:
+            dict_reader = csv.DictReader(f)
+            rows_dicts = [row for row in dict_reader]
+        with open(csv_filename, newline='') as f:
+            next(f) # skip header row
+            rows_raw = [row.strip() for row in f]
+    except FileNotFoundError:
+        print(f"File does not exist: {csv_filename}")
+        return False
+
+    row_data = (Row(raw=raw, row_dict=row_dict) 
+                for (raw, row_dict) in zip(rows_raw, rows_dicts))
+
+    for row in row_data:
+        if eval_search_query(row, query):
+            print(row.raw)
+
+    return True
 
 
 if __name__ == "__main__":
